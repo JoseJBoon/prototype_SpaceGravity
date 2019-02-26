d55ae3972848acb5cf857ac7a8b768f0457cb64e
josejboon <josejboon@gmail.com>
2019-02-26 19:16:40 +0100

    Unity project Init

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpForce = 500;
+
+    Rigidbody2D m_rigidbody;
+
+    public void Move(float horizontal)
+    {
+
+    }
+
+    public void Jump()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerController : MonoBehaviour
+{
+    CharacterController2D controller;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
