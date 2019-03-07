091579a263d88c880ea68133730233851e98a96f
josejboon <josejboon@gmail.com>
2019-03-07 22:31:00 +0100

    Follow Camera Behaviour

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    public Transform target;
+
+    Vector3 offset;
+
+    void Awake()
+    {
+        if(!target)
+        {
+            Debug.LogWarning($"Warning: FollowTarget at {name} behaviour has no target to follow!");
+            Destroy(this);
+        }
+        
+        offset = transform.position - target.position;
+    }
+
+    void LateUpdate()
+    {
+        if(!target)
+            return;
+        
+        transform.position = target.position + offset;
+    }
+}
