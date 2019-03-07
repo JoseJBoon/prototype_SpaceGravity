7da27bb7022aa5ce125f3c85c0c8dae444b06e36
josejboon <josejboon@gmail.com>
2019-03-07 23:55:54 +0100

    Battery Behaviour Added

---
+++ Assets/Scripts/BatterySocketBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class BatterySocketBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    PowerDeviceBehaviour powerDevice;
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(!other.CompareTag("Battery") || !powerDevice)
+            return;
+
+        other.enabled = true;
+    }    
+
+}
