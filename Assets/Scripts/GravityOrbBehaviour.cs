7bea63f20286b7bbf499d78b9effe024e25de9c7
josejboon <josejboon@gmail.com>
2019-03-07 22:32:37 +0100

    Gravity Orb color distinction for repel and attract

--- Assets/Scripts/GravityOrbBehaviour.cs
+++ Assets/Scripts/GravityOrbBehaviour.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 public class GravityOrbBehaviour : MonoBehaviour
 {
     public float breakdownTime = 2.0f;
+    public Color attractColor = new Color(0, 0.695f, 0.99f, 0.256f);
+    public Color repelColor = new Color(0.604f, 0.082f, 0.0432f, 0.256f);
 
     Collider2D collider;
     Rigidbody2D rigidbody;
     PointEffector2D pointEffector;
+    SpriteRenderer spriteRendererEffector;
     float startForce;
     float endForce;
     float timer;
@@ -20,11 +23,15 @@ public class GravityOrbBehaviour : MonoBehaviour
         collider = GetComponent<Collider2D>();
         rigidbody = GetComponent<Rigidbody2D>();
         pointEffector = GetComponentInChildren<PointEffector2D>();
+        spriteRendererEffector = GetComponentsInChildren<SpriteRenderer>()[1]; // :(
+
+            Debug.Log(Color.red);
 
         startForce = pointEffector.forceMagnitude;
         endForce = 0.0f;
         breakDown = false;
         timer = 0.0f;
+        spriteRendererEffector.color = attractColor;
     }
 
     void Update()
@@ -46,8 +53,13 @@ public class GravityOrbBehaviour : MonoBehaviour
         rigidbody.velocity = Vector2.zero;
         collider.enabled = false;
     }
+
+    public void ReverseForceMagnitude()
+    {
+        pointEffector.forceMagnitude = -pointEffector.forceMagnitude;
+        spriteRendererEffector.color = repelColor;
+    }
     
     public void LaunchOrb(Vector2 force) => rigidbody.AddForce(force);
-    public void ReverseForceMagnitude() => pointEffector.forceMagnitude = -pointEffector.forceMagnitude;
     public void DestroyOrb() => breakDown = true;
 }
