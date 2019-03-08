f523de1e18be218e2dfa51942a4dc1727d1a4c6d
josejboon <josejboon@gmail.com>
2019-03-08 08:36:15 +0100

    New Power device; Counter gravity behaviour

---
+++ Assets/CounterGravityBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ConstantForce2D))]
+public class CounterGravityBehaviour : MonoBehaviour
+{
+    ConstantForce2D constantForce;
+
+    void Awake() 
+    {
+        constantForce = GetComponent<ConstantForce2D>();
+        constantForce.enabled = false;   
+    }
+
+    public void EnableConstantForce(Vector2 counterGravity)
+    {
+        constantForce.enabled = true;
+        constantForce.force = counterGravity;
+    }
+
+    public void DisableConstantForce(Vector2 counterGravity)
+    {
+        constantForce.enabled = false;
+    }
+}
---
+++ Assets/GravityPowerDevice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AreaEffector2D))]
+public class GravityPowerDevice : PowerDeviceBehaviour
+{
+    AreaEffector2D areaEffector;
+
+    void Awake() 
+    {
+        areaEffector = GetComponent<AreaEffector2D>();
+    }
+
+    void OnEnable() 
+    {
+        areaEffector.enabled = true;
+    }
+
+    void OnDisable()
+    {
+        areaEffector.enabled = false;
+    }
+}
