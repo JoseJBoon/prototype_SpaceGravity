d0004fe396cf08fcd40fa86968a7ddf73ab2effb
josejboon <josejboon@gmail.com>
2019-03-08 08:36:31 +0100

    Next level behaviour

---
+++ Assets/Scripts/CompleteLevelBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CompleteLevelBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    string nextLevel = "Level01";
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if(!other.CompareTag("Player"))
+            return;
+
+        SceneManager.LoadScene(nextLevel);
+    }
+}
