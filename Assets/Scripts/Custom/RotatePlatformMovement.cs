a60d7e2364b22e53ddba964e99cacc670e846c5c
josejboon <josejboon@gmail.com>
2019-03-15 00:41:23 +0100

    Platform movement scripts for testing the new character controller

---
+++ Assets/Scripts/Custom/RotatePlatformMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom
+{
+    public class RotatePlatformMovement : MonoBehaviour
+    {
+        public float speed = 1.0f;
+        public float distance = 3.0f;
+
+        Rigidbody2D rb2d;
+        Vector2 startPostion;
+
+        float x;
+        float y;
+
+        void Awake()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            startPostion = transform.position;
+        }
+
+        void FixedUpdate()
+        {
+            x = Mathf.Sin(Time.time * speed) * distance;
+            y = Mathf.Cos(Time.time * speed) * distance;
+
+            rb2d.MovePosition(startPostion + new Vector2(x, y));
+
+            x = y = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Custom/SidewaysPlatformMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Custom
+{
+    public class SidewaysPlatformMover : MonoBehaviour
+    {
+        public float speed = 1.0f;
+        public float distance = 3.0f;
+        public bool moveInXDirection = true;
+
+        Rigidbody2D rb2d;
+        Vector2 startPosition;
+        float movement;
+
+        void Awake()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+            startPosition = transform.position;
+        }
+
+        void FixedUpdate()
+        {
+            movement = Mathf.Sin(Time.time * speed) * distance;
+            Vector2 move = Vector2.zero;
+
+            if (moveInXDirection)
+                move.x = movement;
+            else
+                move.y = movement;
+
+            rb2d.MovePosition(startPosition + move);
+
+            movement = 0;
+        }
+    }
+
+}
+
